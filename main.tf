9dea0db8b308b8145591cde5a91ba15aa523dd4c
phobyjun <phobyjun@gmail.com>
2022-01-29 23:41:09 +0900

    initial k8s release

--- main.tf
+++ main.tf
@@ -27,5 +27,21 @@ module "aws-network" {
   private_subnet_b_cidr = "10.10.192.0/18"
 }
 # EKS Configuration
+module "aws-eks" {
+  source = "github.com/phobyjun/module-aws-kubernetes"
 
+  ms_namespace       = "microservices"
+  env_name           = local.env_name
+  aws_region         = local.aws_region
+  cluster_name       = local.k8s_cluster_name
+  vpc_id             = module.aws-network.vpc_id
+  cluster_subnet_ids = module.aws-network.subnet_ids
+
+  nodegroup_subnet_ids     = module.aws-network.private_subnet_ids
+  nodegroup_disk_size      = "20"
+  nodegroup_instance_types = ["t3.medium"]
+  nodegroup_desired_size   = 1
+  nodegroup_min_size       = 1
+  nodegroup_max_size       = 3
+}
 # GitOps Configuration
